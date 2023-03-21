d86149fc2a463ea3aa60756d46db8a8a7b750b11
myusername <myemail@gmail.com>
2023-03-21 02:29:56 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print all alphabet in lowercase 10 times
+ *
+ *Return: always 0 (success)
+*/
+
+void print_alphabet_x10(void)
+{
+	int i;
+	int j;
+
+	for (i = 1; i <= 10; i++)
+	{
+		for (j = 97; j <= 122; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+}
